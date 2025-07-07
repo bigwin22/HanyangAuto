b9e8798fd553081a673b757e73a0de2e773332ee
kth88 <gogletaehyung0808@naver.com>
2025-07-07 16:37:01 +0900

    WebDriverWait 모듈의 import 경로 수정

--- automation.py
+++ automation.py
@@ -1,7 +1,7 @@
 from typing import Dict, List, Union
 from utils.selenium_utils import init_driver, obj_click
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
 from selenium.common.exceptions import TimeoutException, NoSuchElementException, WebDriverException
