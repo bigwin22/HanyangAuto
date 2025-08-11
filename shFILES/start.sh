c6ec299d31492dba24279e4963356b25dd5b72c8
kth88 <gogletaehyung0808@naver.com>
2025-08-11 21:53:39 +0900

    refactor(deploy): Update start.sh to use environment variables for project directory and Docker image, and enhance docker-compose commands

--- shFILES/start.sh
+++ shFILES/start.sh
@@ -5,8 +5,8 @@
 set -e
 
 # --- 사용자 설정 필요한 부분 ---
-PROJECT_DIR="/home/kth88/services/hanyang-auto" # 1. 실제 서버의 프로젝트 절대 경로로 수정하세요.
-DOCKER_IMAGE="kth88/hanyang-automation:latest" # 2. 실제 Docker Hub 이미지 주소로 수정하세요.
+PROJECT_DIR=${PROJECT_DIR} # 깃허브 액션에서 환경변수로 입력할 것이다.
+DOCKER_IMAGE=${DOCKER_IMAGE} # 2. 실제 Docker Hub 이미지 주소로 수정하세요. 깃허브 액션에서 환경변수로 입력할 것이다.
 # --------------------------
 
 echo "배포를 시작합니다..."
@@ -14,17 +14,13 @@ echo "배포를 시작합니다..."
 # 1. 프로젝트 디렉토리로 이동
 cd "$PROJECT_DIR" || { echo "프로젝트 디렉토리를 찾을 수 없습니다: $PROJECT_DIR"; exit 1; }
 
-# 2. Git 저장소에서 최신 코드를 가져옴
-echo "Git에서 최신 코드를 가져옵니다..."
-git pull origin main
-
 # 3. Docker Hub에서 최신 이미지를 가져옴
 echo "Docker Hub에서 최신 이미지를 가져옵니다: $DOCKER_IMAGE"
 docker pull "$DOCKER_IMAGE"
 
 # 4. Docker Compose로 서비스 재시작
 echo "Docker Compose로 서비스를 재시작합니다..."
-docker-compose down
-docker-compose up -d
+docker-compose down --remove-orphans -v
+docker-compose up -d --pull always --no-build
 
 echo "배포가 성공적으로 완료되었습니다."
